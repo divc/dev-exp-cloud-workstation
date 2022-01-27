9bd7c6601523df3c50c38364a3b33056d7bb8b8c
richardtz12 <richardtz12@gmail.com>
2022-01-27 12:06:20 -0500

    Updating this to take in Region dynamically

--- scripts/create_cluster.sh
+++ scripts/create_cluster.sh
@@ -1,11 +1,11 @@
 curl -H "Authorization: Bearer $(gcloud auth print-access-token)" \
      -H "Content-Type: application/json" \
      -d @cluster.json \
-https://workstations.googleapis.com/v1alpha1/projects/${PROJECT}/locations/us-central1/workstationClusters?workstation_cluster_id=${CLUSTER_ID}
+https://workstations.googleapis.com/v1alpha1/projects/${PROJECT}/locations/${REGION}/workstationClusters?workstation_cluster_id=${CLUSTER_ID}
 echo Your cluster is being created. This script will terminate once it is available.
 while (curl -s -H "Authorization: Bearer $(gcloud auth print-access-token)" \
      -H "Content-Type: application/json" \
-https://workstations.googleapis.com/v1alpha1/projects/${PROJECT}/locations/us-central1/workstationClusters/${CLUSTER_ID} | grep -q reconciling)
+https://workstations.googleapis.com/v1alpha1/projects/${PROJECT}/locations/${REGION}/workstationClusters/${CLUSTER_ID} | grep -q reconciling)
 do
 	sleep 120
 done
